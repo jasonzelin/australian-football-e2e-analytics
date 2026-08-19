0ba87f9b90c9c59e0f88065739b0cfc460a03e25
jasonzelin <jr9808@gmail.com>
2026-08-19 16:53:04 +0800

    feat: add API limit retry mechanism and data extraction to intermediary form

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import json
 
 import utils
 
+import vertexai
+from vertexai.generative_models import GenerativeModel, GenerationConfig
+
 dotenv.load_dotenv()
 api_key = os.getenv("API_KEY")
 
@@ -23,7 +26,6 @@ with open(config_path) as f:
 
 # Main Function
 def main():
-    dataframes_dict = {}
     logger = utils.setup_logging(CONFIG["log_dir"])
     
     # 1. Download
@@ -38,7 +40,7 @@ def main():
                 url_params=CONFIG["endpoints"][e],
                 endpoint=e,
                 timeout=CONFIG["request_timeout_seconds"],
-                output_dir=CONFIG["output_dir"],
+                output_dir=CONFIG["raw_output_dir"],
                 target_folder=e,
                 target_file=e,
                 logger=logger,
@@ -51,26 +53,19 @@ def main():
                     url_params=p,
                     endpoint=e,
                     timeout=CONFIG["request_timeout_seconds"],
-                    output_dir=CONFIG["output_dir"],
+                    output_dir=CONFIG["raw_output_dir"],
                     target_folder=e,
                     target_file='_'.join([f'{k}={v}' for k, v in p.items()]),
                     logger=logger,
                     api_key=api_key # type: ignore # used  to suppress mypy error about Optional[str] vs str
                 )
 
-    # # 2. Extract
-    # raw_bytes_dir = Path(__file__).parent /  './data/raw'
-    # raw_bytes = list(raw_bytes_dir.iterdir())
-
-    # for r in raw_bytes:
-    #     logger.info(f"Extracting data from {r.name}")
-    #     dataframe = utils.extract_afl_data(
-    #         data_bytes=r.read_bytes(),
-    #         output_dir=CONFIG["output_dir"],
-    #         target_file=r.name.split('.')[0],
-    #         logger=logger,
-    #     )
-    #     dataframes_dict[r.name.split('.')[0]] = dataframe
+    # 2. Extract
+    utils.extract_afl_data(
+        raw_data_dir=CONFIG["raw_output_dir"],
+        output_dir=CONFIG["intermediate_output_dir"],
+        logger=logger,
+    )
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -4,7 +4,9 @@ import json
 import logging
 import zipfile
 from datetime import datetime
+import time
 from pathlib import Path
+import re
 
 import pandas as pd
 import requests
@@ -57,19 +59,27 @@ def download_afl_data(
 
     logger.info(f"Downloading raw data from: {full_url}")
     response = requests.get(full_url, headers=headers, timeout=timeout)
-    response.raise_for_status()                           # raises on 4xx/5xx
-    size_mb = len(response.content) / (1024)
-    logger.info(f"Download complete — {size_mb:.1f} kB received")
-    if json.loads(response.text)['errors'] != []:
+    response.raise_for_status() # raises on 4xx/5xx
+    if re.search(r'rateLimit', str(json.loads(response.text)['errors'])):
+        logger.error(f"API returned errors: {json.loads(response.text)['errors']}")
+        time.sleep(60)  # wait 60 seconds before retrying
+
+        response = requests.get(full_url, headers=headers, timeout=timeout)
+        response.raise_for_status() # raises on 4xx/5xx
+
+    elif json.loads(response.text)['errors'] != []:
         logger.error(f"API returned errors: {json.loads(response.text)['errors']}")
 
-    # Path(output_dir).mkdir(parents=True, exist_ok=True)
-    output_file = Path(f"{output_dir}/{target_folder}/{target_file}.json")
-    output_file.parent.mkdir(parents=True, exist_ok=True)
-    output_file.write_bytes(response.content)
+    else:
+        size_mb = len(response.content) / (1024)
+        logger.info(f"Download complete — {size_mb:.1f} kB received")
 
-    dataframe = pd.DataFrame(json.loads(response.content)['response'])
-    logger.info(f"Loaded '{target_folder}/{target_file}' — {len(dataframe):,} rows, {len(dataframe.columns)} columns")
+        output_file = Path(f"{output_dir}/{target_folder}/{target_file}.json")
+        output_file.parent.mkdir(parents=True, exist_ok=True)
+        output_file.write_bytes(response.content)
+
+        dataframe = pd.DataFrame(json.loads(response.content)['response'])
+        logger.info(f"Loaded '{target_folder}/{target_file}' — {len(dataframe):,} rows, {len(dataframe.columns)} columns")
 
     return None
 
@@ -77,21 +87,28 @@ def download_afl_data(
 # STEP 2 — Inspect ZIP contents and extract target files
 # ---------------------------------------------------------------------------
 def extract_afl_data(
-        data_bytes: bytes,
+        raw_data_dir: str,
         output_dir: str,
-        target_folder: str,
-        target_file: str,
         logger: logging.Logger,
-    ) -> pd.DataFrame:
+    ) -> None:
     """
     Open the bytes file, then extract only the target files into pandas DataFrames.
     Returns a DataFrame.
     """
     Path(output_dir).mkdir(parents=True, exist_ok=True)
-    output_file = Path(output_dir) / f"{target_folder} / {target_file}.json"
-    output_file.write_bytes(data_bytes)
 
-    dataframe = pd.DataFrame(json.loads(data_bytes)['response'])
-    logger.info(f"Loaded '{target_file}' — {len(dataframe):,} rows, {len(dataframe.columns)} columns")
+    raw_bytes_folders = list(Path(raw_data_dir).iterdir())
 
-    return dataframe
+    for r in raw_bytes_folders:
+        logger.info(f"Extracting data from {r.name}")
+        raw_bytes_files = list(r.iterdir())
+        df = pd.DataFrame()
+
+        for f in raw_bytes_files:
+            df_loop = pd.DataFrame(json.loads(f.read_bytes())['response'])
+            df = pd.concat([df, df_loop], ignore_index=True) # type: ignore
+
+        df.to_csv(Path(f"{output_dir}/{r.name}.csv"), index=False)
+        logger.info(f"Loaded '{r}' — {len(df):,} rows, {len(df.columns)} columns")
+
+    return None
