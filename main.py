18f577371eb8a73df76ab271ac7bb4341db0d5b9
jasonzelin <jr9808@gmail.com>
2026-08-20 14:13:11 +0800

    refactor: refactor the codes to adjust to the other refactorings

--- main.py
+++ main.py
@@ -1,69 +1,53 @@
 import dotenv
 import os
-from pathlib import Path
-import json
 
-import utils
-
-import vertexai
-from vertexai.generative_models import GenerativeModel, GenerationConfig
+from utils import logger as app_logger, data_extraction
+import config
 
 dotenv.load_dotenv()
-api_key = os.getenv("API_KEY")
-
-
-# Load config from root directory
-config_path = Path(__file__).parent / "config.json"
-
-if not config_path.exists():
-    raise FileNotFoundError(
-        f"config.json not found at {config_path}. "
-        "Create or copy json values to config.json and fill in your values."
-    )
-
-with open(config_path) as f:
-    CONFIG = json.load(f)
+afl_api_key = os.getenv("AFL_API_KEY")
+gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 # Main Function
 def main():
-    logger = utils.setup_logging(CONFIG["log_dir"])
+    logger = app_logger.setup_logging(config.LOG_DIR)
     
     # 1. Download
-    for e in CONFIG["endpoints"].keys():
+    for e in config.ENDPOINTS.keys():
         logger.info("Starting AFL data ingestion")
 
-        url_params = CONFIG["endpoints"][e]
+        url_params = config.ENDPOINTS[e]
         if url_params == 'N/A':
             url_params = None
-            zip_byte = utils.download_afl_data(
-                url=f"{CONFIG["afl_url"]}",
-                url_params=CONFIG["endpoints"][e],
+            data_extraction.download_afl_data(
+                url=f"{config.AFL_URL}",
+                url_params=config.ENDPOINTS[e],
                 endpoint=e,
-                timeout=CONFIG["request_timeout_seconds"],
-                output_dir=CONFIG["raw_output_dir"],
+                timeout=config.REQUEST_TIMEOUT_SECONDS,
+                output_dir=config.RAW_OUTPUT_DIR,
                 target_folder=e,
                 target_file=e,
                 logger=logger,
-                api_key=api_key # type: ignore # used  to suppress mypy error about Optional[str] vs str
+                api_key=afl_api_key # type: ignore # used  to suppress mypy error about Optional[str] vs str
             )
         else:
             for p in url_params:
-                zip_byte = utils.download_afl_data(
-                    url=f"{CONFIG["afl_url"]}",
+                data_extraction.download_afl_data(
+                    url=f"{config.AFL_URL}",
                     url_params=p,
                     endpoint=e,
-                    timeout=CONFIG["request_timeout_seconds"],
-                    output_dir=CONFIG["raw_output_dir"],
+                    timeout=config.REQUEST_TIMEOUT_SECONDS,
+                    output_dir=config.RAW_OUTPUT_DIR,
                     target_folder=e,
                     target_file='_'.join([f'{k}={v}' for k, v in p.items()]),
                     logger=logger,
-                    api_key=api_key # type: ignore # used  to suppress mypy error about Optional[str] vs str
+                    api_key=afl_api_key # type: ignore # used  to suppress mypy error about Optional[str] vs str
                 )
 
     # 2. Extract
-    utils.extract_afl_data(
-        raw_data_dir=CONFIG["raw_output_dir"],
-        output_dir=CONFIG["intermediate_output_dir"],
+    data_extraction.extract_afl_data(
+        raw_data_dir=config.RAW_OUTPUT_DIR,
+        output_dir=config.INTERMEDIATE_OUTPUT_DIR,
         logger=logger,
     )
 
