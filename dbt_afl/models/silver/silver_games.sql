3bdc457c6b34bc71648e383abface379be105ef6
jasonzelin <jr9808@gmail.com>
2026-09-04 13:38:36 +0800

    fix: apply LAX_INT64 function to directly cast JSON extract as integer

--- dbt_afl/models/silver/silver_games.sql
+++ dbt_afl/models/silver/silver_games.sql
@@ -14,8 +14,8 @@ with source as (
         ,attendance
         ,JSON_EXTRACT(safe.parse_json(regexp_replace(status, r"\'", '"')), '$.long') as long_status
         ,JSON_EXTRACT(safe.parse_json(regexp_replace(status, r"\'", '"')), '$.short') as short_status
-        ,JSON_EXTRACT(safe.parse_json(regexp_replace(teams, r"\'", '"')), "$.home.id") as home_team_id
-        ,JSON_EXTRACT(safe.parse_json(regexp_replace(teams, r"\'", '"')), "$.away.id") as away_team_id
+        ,LAX_INT64(JSON_EXTRACT(safe.parse_json(regexp_replace(teams, r"\'", '"')), "$.home.id")) as home_team_id
+        ,LAX_INT64(JSON_EXTRACT(safe.parse_json(regexp_replace(teams, r"\'", '"')), "$.away.id")) as away_team_id
         ,JSON_EXTRACT(safe.parse_json(regexp_replace(scores, r"\'", '"')), "$.home") as home_scores
         ,JSON_EXTRACT(safe.parse_json(regexp_replace(scores, r"\'", '"')), "$.away") as away_scores
     from
