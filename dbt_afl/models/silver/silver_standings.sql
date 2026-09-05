64d3f4b8679077a93a2e64da65ba95efed8a0582
jasonzelin <jr9808@gmail.com>
2026-09-05 21:27:38 +0800

    refactor: separate JSON values in silver standings as multiple columns

--- dbt_afl/models/silver/silver_standings.sql
+++ dbt_afl/models/silver/silver_standings.sql
@@ -6,8 +6,12 @@ select
     position
     ,LAX_INT64(JSON_EXTRACT(safe.parse_json(regexp_replace(team, r"\'", '"')), "$.id")) as team_id
     ,pts
-    ,LAX_INT64(JSON_EXTRACT(safe.parse_json(regexp_replace(games, r"\'", '"')), "$.id")) as games_id
-    ,LAX_INT64(JSON_EXTRACT(safe.parse_json(regexp_replace(points, r"\'", '"')), "$.id")) as points_id
+    ,LAX_INT64(JSON_EXTRACT(safe.parse_json(regexp_replace(games, r"\'", '"')), "$.played")) as played_games
+    ,LAX_INT64(JSON_EXTRACT(safe.parse_json(regexp_replace(games, r"\'", '"')), "$.win")) as win_games
+    ,LAX_INT64(JSON_EXTRACT(safe.parse_json(regexp_replace(games, r"\'", '"')), "$.drawn")) as drawn_games
+    ,LAX_INT64(JSON_EXTRACT(safe.parse_json(regexp_replace(games, r"\'", '"')), "$.lost")) as lost_games
+    ,LAX_INT64(JSON_EXTRACT(safe.parse_json(regexp_replace(points, r"\'", '"')), '$.for')) as for_points
+    ,LAX_INT64(JSON_EXTRACT(safe.parse_json(regexp_replace(points, r"\'", '"')), '$.against')) as against_points
     ,last_5
 from
     source
